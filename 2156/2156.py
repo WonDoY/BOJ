4ed845f1fa251191f314090e551b1776f8f1923c
WonDoY <wondy1128@naver.com>
2016-05-09 15:48:21 +0900

    2156 solve
    py dp

--- 2156/2156.py
+++ 2156/2156.py
@@ -9,7 +9,12 @@ for x in range(T):
     if(s==0):
         step[1].append(max(step[1][2+x],step[2][2+x]))
         step[2].append(max(step[1][2+x],step[2][2+x]))
-    else :        
+    elif(x<=2):       
+        step[1].append(max(max(step[1][:x+1]),max(step[2][:x+1]))+step[0][2+x]+step[0][3+x])
+        step[2].append(max(max(step[1][:x+2]),max(step[2][:x+2]))+step[0][3+x])
+    else:
+        step[1].append(max(max(step[1][x-3:x+1]),max(step[2][:x+1]))+step[0][2+x]+step[0][3+x])
+        step[2].append(max(max(step[1][x-3:x+2]),max(step[2][:x+2]))+step[0][3+x])
     if(res < step[1][3+x]): res = step[1][3+x]
     if(res < step[2][3+x]): res = step[2][3+x]
 print(res)
