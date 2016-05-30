59133b1963ddac7f1b1bb75e7bd13cd7564270ff
WonDoY <wondy1128@naver.com>
2016-05-30 22:55:35 +0900

    Fibonacci
    Python

--- 2748/2748.py
+++ 2748/2748.py
@@ -1,11 +1,10 @@
-#2747 피보나치 수
-#2748 2
+#2748 피보나치 2
 def fibo(n):
     flist=[0,1]
     cnt=2
     while(cnt<=n):
         flist.append(flist[cnt-1]+flist[cnt-2])
         cnt+=1
-    print(flist[n]%1000000)
+    print(flist[n])
 fibo(int(input()))
     
