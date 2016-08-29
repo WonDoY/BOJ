55cc5e08c3bb8f79358c1953ff9d48e57b08a9d8
WonDoY <wondy1128@naver.com>
2016-08-29 23:25:28 +0900

    printf
    basic print

---
+++ 2741/2741.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdio>
+
+int main(){
+    int input, output;
+    output = 0 ;
+    scanf("%d",&input);
+    while(input--){        
+        output++;
+        printf("%d\n",output);
+    }
+    return 0;
+}
